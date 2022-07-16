273522f4c31e931154522371f0027556b533c52a
Flávio Rodrigo Leonel <frleonel@gmail.com>
2022-07-16 13:37:57 -0300

    security

--- configure.php
+++ configure.php
@@ -2,7 +2,7 @@
 
 define('OUTBOUNT_INTERFACE', 'eth7');
 define('IP_PRI_START', '100.64.0.0');
-define('IP_PUB_START', '45.225.86.136');
-define('IP_PUB_STOP',  '45.225.86.142');
+define('IP_PUB_START', '200.225.86.136');
+define('IP_PUB_STOP',  '200.225.86.142');
 define('IP_CGNAT', '128');
 
